a9d7be81ed4d75e0173f7aff1c2700545863c95e
carvalhaus <ooliveira.joaop@gmail.com>
2023-09-04 23:47:02 -0300

    feat: project completed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import qrCodeImage from "./assets/image-qr-code.png";
+
+function App() {
+  return (
+    <main>
+      <div className="wrapper">
+        <img src={qrCodeImage} alt="QR Code Image" />
+        <div className="text">
+          <p>Improve your front-end skills by building projects</p>
+          <small>
+            Scan the QR code to visit Frontend Mentor and takeyour coding skills
+            to the next level
+          </small>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
